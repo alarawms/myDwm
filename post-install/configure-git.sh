c80c9226f7f1902089a8f1ef936f3c66380d6684
alarawms <alarawi.m@gmail.com>
2022-12-23 09:46:01 +0300

    modid

--- post-install/configure-git.sh
+++ post-install/configure-git.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # configure git
-git config --global user.name "Brahim Hamdouni"
-git config --global user.email "brahim@hamdouni.com"
+git config --global user.name "alarawms"
+git config --global user.email "alarawi.m@gmail.com"
 git config --global push.default simple
